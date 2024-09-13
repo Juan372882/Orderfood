b19ca7086e207effb54668841e902cd81899b1da
Juan <jmmartinez4817@misena.edu.co>
2024-09-13 12:49:36 -0500

    Explor Menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise, one delicious meal at a time</p>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise, one delicious meal at a time.</p>
         <button>View Menu</button>
       </div>
     </div>
