bb0cc0c0d1df843132e41bc86d671418aeba88bd
Juan <jmmartinez4817@misena.edu.co>
2024-09-23 12:32:59 -0500

    remove items from cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,11 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
-
-    const{cardItems,food_list,removeFromCart} = useContext(StoreContext);
+  const { cardItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
-    <div className='cart'>
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -18,19 +17,26 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cardItems[item._id]>0)
-          {
+        {food_list.map((item, index) => {
+          if (cardItems[item._id] > 0) {
             return (
-              <div className='cart-item-title cart-items-item'>
-                <p>{item.name}</p>
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cardItems[item._id]}</p>
+                  <p>${item.price * cardItems[item._id]}</p>
+                  <p onClick={()=>removeFromCart(item._id)} className="cross">x</p>
+                </div>
+                <hr />
               </div>
-            )
+            );
           }
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
