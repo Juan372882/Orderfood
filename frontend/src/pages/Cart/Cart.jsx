428913f962e35e86310de31eec314fa26b1fc6d9
Juan <jmmartinez4817@misena.edu.co>
2024-09-23 12:07:04 -0500

    redirect img

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
 
     const{cardItems,food_list,removeFromCart} = useContext(StoreContext);
@@ -8,13 +9,16 @@ const Cart = () => {
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
-          <p></p>
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
         </div>
+        <br />
+        <hr />
+
       </div>
     </div>
   )
